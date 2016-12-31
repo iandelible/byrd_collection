3da5ccfe62625874102f31bf2cac5aae2952a5dd
Ian Pan <ianpan@hotmail.com>
2016-12-31 02:25:21 +0000

    form submits and random quote on index page

--- app/controllers/reasons_controller.rb
+++ app/controllers/reasons_controller.rb
@@ -1,9 +1,20 @@
 class ReasonsController < ApplicationController
   def index
-    
+    @reason = Reason.order("RANDOM()").first
   end
-  
+
   def new
     @reason = Reason.new
+  end
+
+  def create
+    Reason.create(reason_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def reason_params
+    params.require(:reason).permit(:advantage, :reason)
   end
 end
--- app/models/reason.rb
+++ app/models/reason.rb
@@ -1,3 +1,4 @@
 class Reason < ActiveRecord::Base
-    
+  validates :advantage, presence: true, length: {maximum: 7, minimum: 4}
+  validates :reason, presence: true
 end
